a95f6623af234f24a8934ed4396a653152054f6c
Hellharicot <vf.marchal@gmail.com>
2026-01-09 18:00:35 +0100

    feat: adds and animate a textual logo component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Logo } from './logo/logo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Logo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('GrosFlacon');
 }
