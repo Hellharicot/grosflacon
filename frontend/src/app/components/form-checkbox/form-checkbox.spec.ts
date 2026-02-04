9795f42535d43e49145774186f9d28428c38686d
Hellharicot <vf.marchal@gmail.com>
2026-02-04 14:14:43 +0100

    fix: corrects import in form-checkbox.spec.ts

--- frontend/src/app/components/form-checkbox/form-checkbox.spec.ts
+++ frontend/src/app/components/form-checkbox/form-checkbox.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormCheckbox } from './form-checkbox.component';
+import { FormCheckboxComponent } from './form-checkbox.component';
 
 describe('FormCheckbox', () => {
   let component: FormCheckbox;
