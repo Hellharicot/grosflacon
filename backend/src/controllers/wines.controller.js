ed3e33cad3d5af54cbd7e19947b4ee5a2c3acaff
Hellharicot <vf.marchal@gmail.com>
2026-03-14 20:41:50 +0100

    feat: adds api routes for getting and creating wines

--- backend/src/controllers/wines.controller.js
+++ backend/src/controllers/wines.controller.js
@@ -1,8 +1,28 @@
-import { query, executeQuery } from "../shared/db.js";
+import { executeQuery } from "../shared/db.js";
 
 export const getWines = async (req, res) => {
   const sql = `SELECT * FROM wines.wine`;
   await executeQuery(req, res, sql, "wines");
+};
+
+export const getColors = async (req, res) => {
+  const sql = `SELECT * FROM wines.color`;
+  await executeQuery(req, res, sql, "color");
+};
+
+export const getCountries = async (req, res) => {
+  const sql = `SELECT * FROM wines.country`;
+  await executeQuery(req, res, sql, "country");
+};
+
+export const getRegions = async (req, res) => {
+  const sql = `SELECT * FROM wines.region`;
+  await executeQuery(req, res, sql, "region");
+};
+
+export const getAppellations = async (req, res) => {
+  const sql = `SELECT * FROM wines.appellation`;
+  await executeQuery(req, res, sql, "appellation");
 };
 
 export const createWine = async (req, res) => {
@@ -12,12 +32,5 @@ export const createWine = async (req, res) => {
 			VALUES ($1, $2, $3, $4, $5)
 			RETURNING *`;
   const params = [appellation_id, producer, cuvee, color_id, vintage];
-  await executeQuery(req, res, sql, { params });
-};
-
-export const createColor = async (req, res) => {
-  const { color } = req.body;
-  const sql = `INSERT INTO wines.color (default_name) VALUES ($1) RETURNING *`;
-  const params = [color];
   await executeQuery(req, res, sql, { params });
 };
--- backend/src/endpoints/wines.router.js
+++ backend/src/endpoints/wines.router.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { getWines, createColor } from "../controllers/wines.controller.js";
+import {
+  getWines,
+  createWine,
+  getAppellations,
+  getCountries,
+  getRegions,
+  getColors,
+} from "../controllers/wines.controller.js";
 
 export const winesRouter = Router();
 
 winesRouter.get("/", getWines);
-winesRouter.post("/", createColor);
+winesRouter.post("/", createWine);
 // TODO: winesRouter.get("/:id", getWineById);
-// TODO: winesRouter.get("/country/:country", getCountry);
-// TODO: winesRouter.get("/region/:region", getRegion);
-// TODO: winesRouter.get("/appellation/:appellation", getAppellation);
-// TODO: winesRouter.get("/color/:color", getColor);
+winesRouter.get("/countries", getCountries);
+winesRouter.get("/regions", getRegions);
+winesRouter.get("/appellations", getAppellations);
+winesRouter.get("/colors", getColors);
