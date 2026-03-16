6636275ff162db51d66e3534bec0065f4766cacd
Hellharicot <vf.marchal@gmail.com>
2026-03-16 16:26:32 +0100

    refactor: configures proxy and api service to allow frontend to backend communication

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,20 @@ import { usersRouter } from "./endpoints/users.router.js";
 import { winesRouter } from "./endpoints/wines.router.js";
 import { refsRouter } from "./endpoints/refs.router.js";
 
-const port = process.env.BACKEND_PORT;
+const PORT = process.env.BACKEND_PORT;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  }),
+);
+
 app.use(express.json());
 
+app.use("/api/test", (req, res) => {
+  logger.info(`Route /api/test appelée !`);
+});
 app.use("/api/users", usersRouter);
 app.use("/api/wines", winesRouter);
 app.use("/api/refs", refsRouter);
@@ -19,8 +27,8 @@ app.use("/", (req, res) => {
   res.send("Hello from backend!");
 });
 
-app.listen(port, () => {
-  logger.info(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
 });
 
 export default app;
--- frontend/src/app/pages/new-tasting-note/new-tasting-note.component.ts
+++ frontend/src/app/pages/new-tasting-note/new-tasting-note.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@app/services/api';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormInputComponent } from '@app/components/form-input/form-input.component';
@@ -19,9 +20,24 @@ import { FORM_STEPS } from './formstep';
   templateUrl: './new-tasting-note.html',
   styleUrl: './new-tasting-note.css',
 })
-export class NewTastingNoteComponent {
-  steps = FORM_STEPS;
 
+export class NewTastingNoteComponent implements OnInit {
+  responseData: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getTest().subscribe({
+      next: (data) => {
+        this.responseData = data;
+        console.log('Réponse du backend:', data);
+      },
+      error: (err) => {
+        console.error('Erreur:', err);
+      }
+    });
+  }
+  steps = FORM_STEPS;
   wineIdForm = new FormGroup({
     country: new FormControl<string>(''),
     region: new FormControl<string>(''),
