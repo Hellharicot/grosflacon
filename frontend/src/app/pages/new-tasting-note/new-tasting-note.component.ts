dfef398cde5074b2f34a8c4e61c1f725cce261d3
Hellharicot <vf.marchal@gmail.com>
2026-03-18 16:47:15 +0100

    feat: adds Aromas interface to allow frontend to deal with aromas related criteria

--- frontend/src/app/pages/new-tasting-note/new-tasting-note.component.ts
+++ frontend/src/app/pages/new-tasting-note/new-tasting-note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
 import { ApiService } from '@app/services/api';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,7 +7,21 @@ import { FormInputComponent } from '@app/components/form-input/form-input.compon
 import { FormRadioComponent } from '@app/components/form-radio/form-radio.component';
 import { FormCheckboxComponent } from '@app/components/form-checkbox/form-checkbox.component';
 import { FormAromaSelectorComponent } from '@app/components/form-aroma-selector/form-aroma-selector.component';
-import { FORM_STEPS } from './formstep';
+import { ApiResponse } from '@app/services/api';
+
+export interface Aromas {
+  [category: string]: {
+    [subcategory: string]: string[]
+  }
+};
+
+export interface FormStep {
+  criterion: string;
+  default_description: string;
+  category: string;
+  input_type: string;
+  opts: Aromas[];
+}
 
 @Component({
   selector: 'app-new-tasting-note',
@@ -15,29 +30,17 @@ import { FORM_STEPS } from './formstep';
     FormInputComponent,
     FormRadioComponent,
     FormCheckboxComponent,
-    FormAromaSelectorComponent,
+    //FormAromaSelectorComponent,
+    KeyValuePipe,
   ],
   templateUrl: './new-tasting-note.html',
   styleUrl: './new-tasting-note.css',
 })
 
 export class NewTastingNoteComponent implements OnInit {
-  responseData: any;
+  responseData: ApiResponse | null = null;
+  steps: FormStep[] = [];
 
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit() {
-    this.apiService.getTest().subscribe({
-      next: (data) => {
-        this.responseData = data;
-        console.log('Réponse du backend:', data);
-      },
-      error: (err) => {
-        console.error('Erreur:', err);
-      }
-    });
-  }
-  steps = FORM_STEPS;
   wineIdForm = new FormGroup({
     country: new FormControl<string>(''),
     region: new FormControl<string>(''),
@@ -46,4 +49,27 @@ export class NewTastingNoteComponent implements OnInit {
     color: new FormControl<string>(''),
     year: new FormControl<string>(''),
   });
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getTest().subscribe({
+      next: (response: ApiResponse) => {
+        this.responseData = response;
+        this.steps = this.responseData.data;
+        const aromaStep = this.steps.find(step => step.criterion === 'aromas');
+        const flavorStep = this.steps.find(step => step.criterion === 'flavors');
+        console.log(this.steps);
+        if (aromaStep) {
+          console.log('Aromas: ', aromaStep.opts[0]);
+        }
+        if (flavorStep) {
+          console.log('Flavors: ', flavorStep.opts[0]);
+        }
+      },
+      error: (err) => {
+        console.error('Erreur:', err);
+      }
+    });
+  }
 }
--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,12 +1,19 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormStep } from '@app/pages/new-tasting-note/new-tasting-note.component';
+
+export interface ApiResponse {
+  success: string;
+  data: FormStep[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class ApiService {
   private http = inject(HttpClient);
   getTest() {
-    return this.http.get('/api/refs/criteria');
+    return this.http.get<ApiResponse>('/api/refs/criteria');
   }
 }
